79124ce383e59ac3fad03b9558f0eb2ab161837c
Starfall <jason99123@gmail.com>
2018-01-18 17:33:59 +0800

    java pom and gradle setup

---
+++ Blockchain/src/main/java/voting/blockchain/Main.java
@@ -0,0 +1,14 @@
+package voting.blockchain;
+
+import java.util.Arrays;
+ 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperledger.java.shim.ChaincodeBase;
+import org.hyperledger.java.shim.ChaincodeStub;
+
+public class Main {
+	public static void main(String [] args) {
+		System.out.print("ok");
+	}
+}
